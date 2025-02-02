244e3d7daf1b7f4d993150be3fdb5e5a3d3b9b03
Mathi755 <147333217+Mathi755@users.noreply.github.com>
2025-02-02 22:40:56 +0530

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -391,7 +391,7 @@ className="w-full h-full object-cover"
       <div 
   id="register" 
   className="max-w-7xl mx-auto py-16 px-4 sm:px-6 lg:px-8 bg-white"
-  style={{ backgroundImage: 'url("https://png.pngtree.com/png-clipart/20211219/original/pngtree-abstract-registration-banner-with-yellow-blurred-glass-morphism-effect-png-image_6966389.png")', backgroundSize: 'cover', backgroundPosition: 'center' }}
+  style={{ backgroundImage: 'url("https://static.vecteezy.com/system/resources/previews/002/475/624/large_2x/abstract-yellow-background-free-vector.jpg")', backgroundSize: 'cover', backgroundPosition: 'center' }}
 >
   <div className="text-center mb-12">
     <h2 className="title-font text-3xl font-extrabold text-blue-900 sm:text-4xl">
