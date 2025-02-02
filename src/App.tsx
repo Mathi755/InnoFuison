ea3b3133b0670e5dbe9d4ca0dd9c4149fd2ad85c
Mathi755 <147333217+Mathi755@users.noreply.github.com>
2025-02-02 17:59:00 +0530

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -114,10 +114,10 @@ const App: React.FC = () => {
       <br />
       DEPARTMENT OF DATA SCIENCE AND BUSINESS SYSTEMS
     </h2>
-<div className="flex items-center justify-center h-screen px-4 text-center">
+<div className="flex flex-col items-center justify-center h-auto px-4 text-center gap-y-2">
   <h1 className="title-font text-2xl font-extrabold tracking-widest sm:text-4xl lg:text-8xl leading-tight">
     <span className="text-yellow-400">INNOFUSION</span>  
-    <span className="block text-white">'</span>
+    <span className="text-white">'</span>
     <span className="text-red-600">25</span>
   </h1>
 </div>
