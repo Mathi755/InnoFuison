01be7a0d3b8e43b8b36b258193d1d3b80966e021
Mathi755 <147333217+Mathi755@users.noreply.github.com>
2025-02-13 22:19:37 +0530

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -223,7 +223,7 @@ const App: React.FC = () => {
       {/* Event Poster */}
       <div className="max-w-4xl mx-auto py-16 px-4">
        <img
-  src="https://github.com/Mathi755/InnoFuison/raw/main/src/assets/Innofusion%20Poster.png"
+  src="https://raw.githubusercontent.com/Mathi755/InnoFuison/main/src/assets/2%20(2).png"
   alt="INNOFUSION '25 Poster"
   className="w-full rounded-lg shadow-2xl hover:scale-105 transition-transform duration-300"
 />
