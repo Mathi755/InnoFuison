80807315e34b984f5a0117e67b912a387ca4818c
Mathi755 <147333217+Mathi755@users.noreply.github.com>
2025-02-14 22:39:57 +0530

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -153,28 +153,29 @@ const App: React.FC = () => {
 
 
 
-      {/* Hero Section with Timer */}
+{/* Hero Section with Timer */}
 <div className="bg-gradient-to-r from-blue-900 to-blue-800 text-white">
-  <div className="max-w-full mx-auto py-8 px-8 sm:max-w-screen-xl sm:px-12 lg:px-16 text-center">  
-<h2 className="text-2xl sm:text-3xl md:text-4xl lg:text-5xl font-bold mb-4">
-  SRM INSTITUTE OF SCIENCE AND TECHNOLOGY <br />
-  SCHOOL OF COMPUTING <br />
-  DEPARTMENT OF DATA SCIENCE AND BUSINESS SYSTEMS
-</h2>
-<p className="mt-6 text-xl max-w-2xl mx-auto">
-      In association with <br> 
+  <div className="max-w-full mx-auto py-8 px-8 sm:max-w-screen-xl sm:px-12 lg:px-16 text-center">
+    <h2 className="text-2xl sm:text-3xl md:text-4xl lg:text-5xl font-bold mb-4">
+      SRM INSTITUTE OF SCIENCE AND TECHNOLOGY <br />
+      SCHOOL OF COMPUTING <br />
+      DEPARTMENT OF DATA SCIENCE AND BUSINESS SYSTEMS
+    </h2>
+    <p className="mt-6 text-xl max-w-2xl mx-auto">
+      In association with <br />
     </p>
-  <h3 className="text-2xl sm:text-3xl md:text-4xl lg:text-5xl font-bold mb-4">
-  DSBS STUDENT ASSOCIATION
-</h3>
-    <br>
-<div className="flex flex-col items-center justify-center h-[10vh] py-4 px-4 text-center gap-y-2">
-
-  <h1 className="title-font text-2xl font-extrabold tracking-widest sm:text-4xl lg:text-8xl leading-tight">     
-    <span className="text-yellow-400">INNOFUSION</span>       
-    <span className="text-white">'</span>     
-    <span className="text-red-600">25</span>   
-  </h1> 
+    <h3 className="text-2xl sm:text-3xl md:text-4xl lg:text-5xl font-bold mb-4">
+      DSBS STUDENT ASSOCIATION
+    </h3>
+    <br />
+    <div className="flex flex-col items-center justify-center h-[10vh] py-4 px-4 text-center gap-y-2">
+      <h1 className="title-font text-2xl font-extrabold tracking-widest sm:text-4xl lg:text-8xl leading-tight">
+        <span className="text-yellow-400">INNOFUSION</span>
+        <span className="text-white">'</span>
+        <span className="text-red-600">25</span>
+      </h1>
+    </div>
+  </div>
 </div>
 
     
