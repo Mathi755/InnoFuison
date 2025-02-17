dc8369448dd46249f1e73d835555d75ba26a5e31
Mathi755 <147333217+Mathi755@users.noreply.github.com>
2025-02-17 20:40:55 +0530

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,7 @@
 import React, { useState, useEffect } from 'react';
+import Slider from 'react-slick';
+import "slick-carousel/slick/slick.css"; 
+import "slick-carousel/slick/slick-theme.css";
 
 interface TimeLeft {
   days: number;
